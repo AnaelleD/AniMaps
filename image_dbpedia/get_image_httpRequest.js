4f23a9c8d506d59ce04a2ae704fc23b38cd844d7
AnaelleD <anaelle.dambreville@gmail.com>
2017-12-13 23:14:47 +0100

    Utilisation de Fetch a la place de requeteHTTP pour récuperer image DBpedia

--- image_dbpedia/get_image_httpRequest.js
+++ image_dbpedia/get_image_httpRequest.js
@@ -9,15 +9,17 @@ get_image = function(){
 		 "SELECT ?label_wiki",
 		 "WHERE {dbp:Raphus_cucullatus dbpedia-owl:wikiPageRedirects ?label_wiki }"].join(" ");
 	var query_label_wiki_Url = url+"?query="+ encodeURIComponent(query_label_wiki) +"&format=json"; // Requete SPARQL pour dbpedia
-
-	///// Requete Fetch avec l'Url pour récupérer le nom de la page principale de l'espece /////
-	var label_wiki = "";
-	fetch(query_label_wiki_Url).then(function(response) {
-		return response.json().then(function(json) {
-			label_wiki = json.results;
+	
+	// Requete HTTP avec l'Url pour récupérer le nom de la page principale de l'espece: 
+	var Requete_label_wiki = new XMLHttpRequest();
+	Requete_label_wiki.open("GET", query_label_wiki_Url, true);
+	Requete_label_wiki.send(null);
+	Requete_label_wiki.onreadystatechange = function() {
+		if (Requete_label_wiki.readyState == XMLHttpRequest.DONE) {
+			var json = JSON.parse(Requete_label_wiki.responseText);
+			var label_wiki = json.results;
 			label_wiki = label_wiki["bindings"][0]["label_wiki"]["value"];
-			//console.log(label_wiki);
-
+			
 			///// Récupérer l'image à partir de la page dbpedia principale de l'espece  /////
 			var query_image = [
 				"PREFIX foaf: <http://xmlns.com/foaf/0.1/>",
@@ -25,20 +27,22 @@ get_image = function(){
 				["<",label_wiki,">"].join(""),
 				"foaf:depiction ?image }"].join(" ");
 			var query_image_Url = url+"?query="+ encodeURIComponent(query_image)+"&format=json"; // Requete SPARQL pour dbpedia
-
-			var image = "";
-			fetch(query_image_Url).then(function(response) {
-				return response.json().then(function(json) {
-					image = json.results;
+			
+			// Requete HTTP avec l'Url pour récupérer l'image: 
+			var Requete_image = new XMLHttpRequest();
+			Requete_image.open("GET", query_image_Url, true);
+			Requete_image.send(null);
+			Requete_image.onreadystatechange = function() {
+				if (Requete_image.readyState == XMLHttpRequest.DONE) {
+					var json = JSON.parse(Requete_image.responseText);
+					var image = json.results;
 					image = image["bindings"][0]["image"]["value"];
-					
-					///// On met l'image dans le HTML /////
+
 					var image_wiki = document.getElementById('image_wiki');
 					image_wiki.innerHTML = '<img src="' + image + '"/>';  // Lien vers le HTML
-					//console.log(image_wiki);	
-				}); 
-			});	
-	
-		});
-	});	
+					//console.log(image_wiki);
+				}
+			}
+		}
+	}	
 }
