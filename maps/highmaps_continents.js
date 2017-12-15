9338efedd4af40dd60d476f139d71691ec4fb696
AnaelleD <anaelle.dambreville@gmail.com>
2017-12-15 23:12:01 +0100

    rajout jquery pour rafraichir la liste en cliquant sur la carte + modif get_image pour attraper plus d'erreurs

--- maps/highmaps_continents.js
+++ maps/highmaps_continents.js
@@ -23,48 +23,16 @@ Highcharts.mapChart('container', {
     },
 	
 	plotOptions:{
-        	series:{
-            	point:{
-                	events:{
-                    	click: function(){
-						   
-                        	var id_pays = this["hc-key"];
-							var species = {};
-							var queries = [];
-							var ul = document.getElementById("ul");
-							
-							var query_getspecies = "http://apiv3.iucnredlist.org/api/v3/country/getspecies/"+id_pays+"?token=9bb4facb6d23f48efbf424bb05c0c1ef1cf6f468393bc745d42179ac4aca5fee"; 
-							var query = new XMLHttpRequest();
-							query.open("GET", query_getspecies, true);
-							query.send(null);
-							
-							query.onreadystatechange = function() {
-							if (query.readyState == XMLHttpRequest.DONE) {
-								var json = JSON.parse(query.responseText);
-								var result = json["result"];
-								for(i=0;i<result.length;i++) {
-									var scientific_name = result[i]["scientific_name"];
-									var id_species = result[i]["taxonid"];
-									var li = document.createElement("li");						
-									li.innerHTML = '<a href="#/">'+scientific_name+'</a>'; 
-									li.onclick = (function(i){
-										return function(){
-											var scientific_name_wiki = result[i]["scientific_name"].replace(" ","_");
-											get_image(scientific_name_wiki);
-											
-											var selectedSpeciesName = result[i]["scientific_name"];
-											var selectedSpeciesId = result[i]["taxonid"];
-											get_infospecies(selectedSpeciesId,selectedSpeciesName);
-										}
-									})(i);
-									ul.appendChild(li);									
-								}
-							}								
-                        }
-                    }
-                }
-            }
-        }
+		series:{
+			point:{
+				events:{
+					click: function(){
+						var id_pays = this["hc-key"];
+						get_listspecies(id_pays);
+					}
+				}
+			}
+		}
 	},
 	
 	legend: {
